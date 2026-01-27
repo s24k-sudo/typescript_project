4ee88c0c012c26421f80ad8f098a36b7d6f6df19
s24k-sudo <sohamkulkarni393@gmail.com>
2026-01-27 15:46:25 +0530

    basic changes

--- sample.js
+++ sample.js
@@ -1,2 +1,5 @@
 var msg = "Hello typescript";
 console.log(msg);
+var age = 25;
+var Pname = "Soham";
+console.log("My name is " + Pname + " and my age is " + age);
--- sample.ts
+++ sample.ts
@@ -1,2 +1,6 @@
 let msg:string="Hello typescript";
 console.log(msg);
+let age:number=25;
+let Pname:string="Soham";
+console.log("My name is "+Pname+" and my age is "+age);
+
