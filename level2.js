635468a2313653144c627d8f21a3aa8325c35fe6
s24k-sudo <sohamkulkarni393@gmail.com>
2026-01-27 16:35:14 +0530

    function1

---
+++ level2.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sub(a, b) {
+    return a - b;
+}
+console.log("Substraction :", sub(12, 3));
+function logmessage(msg) {
+    console.log("This is my second function");
+}
---
+++ level2.ts
@@ -0,0 +1,8 @@
+function sub(a: number, b:number):number{
+    return a-b;
+}
+
+console.log("Substraction :", sub(12,3));
+
+
+export{};
