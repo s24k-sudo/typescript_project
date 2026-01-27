6efeffd0d1b1bd71d3ae8702ba37968fedea4fc9
s24k-sudo <sohamkulkarni393@gmail.com>
2026-01-27 17:22:53 +0530

    Arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var multiply = function (a, b) { return a * b; };
+console.log(multiply(100, 4));
---
+++ arrowFunction.ts
@@ -0,0 +1,3 @@
+const multiply = (a:number,b:number):number =>{return a*b};
+console.log(multiply(100,4));
+export{};
